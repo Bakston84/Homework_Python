67c644bc245ffb4d2376e083d824c59ae983a8d8
Bakston84 <Bakston1@yandex.ru>
2023-03-21 11:38:45 +0500

    21032023_1138_task_007

--- Task_007/main.py
+++ Task_007/main.py
@@ -1,1 +1,18 @@
 # Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+def checkingInput(message):
+    while True:
+        n = (input(message))
+        if n.isdigit() == False:
+            print(f'Не верный тип!')
+        else:
+            return n
+
+def integerPowersTwo(n):
+    i = 1
+    while i <= n:
+        print(i, end = f' ')
+        i = i * 2
+   
+n = checkingInput(f'Введите число > ')
+integerPowersTwo(int(n))
